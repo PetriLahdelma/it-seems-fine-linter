f0a5c1b781814324b03b5785ccf6e69bf3e88bbe
Petri Lahdelma <petri.lahdelma@gmail.com>
2026-02-05 01:30:28 +0200

    chore: add src/phrases.ts

---
+++ src/phrases.ts
@@ -0,0 +1,9 @@
+export const defaultPhrases = [
+  { phrase: "it seems fine", severity: 3 },
+  { phrase: "probably ok", severity: 2 },
+  { phrase: "works on my machine", severity: 2 },
+  { phrase: "should be fine", severity: 2 },
+  { phrase: "yolo", severity: 5 },
+  { phrase: "ship it", severity: 3 },
+  { phrase: "can't repro", severity: 2 }
+];
